937bb81380ccc3491dfce00cc3fc0de46a1c5b6f
warlordjones <warlordjones@gmail.com>
2014-03-20 15:40:29 +0000

    Lots of Crafting Items for clockwork stuff.
    Textures are placeholders, and recipies using this stuff coming soon to a github near you!

--- common/steamcraft/common/core/handler/recipe/ModRecipes.java
+++ common/steamcraft/common/core/handler/recipe/ModRecipes.java
@@ -1,10 +1,13 @@
 package common.steamcraft.common.core.handler.recipe;
 
+import java.util.Random;
+
 import common.steamcraft.common.block.ModBlocks;
 import common.steamcraft.common.item.ModItems;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -13,9 +16,13 @@ public class ModRecipes {
 	public static void initRecipes() 
 	{
 		//Blocks
-		GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.castIronLampOff), "ccc", "flf", " f ", 'c', ModBlocks.blockCastIron, 'f', ModBlocks.railingCastIron, 'l', ModItems.lightBulb);
+		GameRegistry.addShapedRecipe(new ItemStack(ModBlocks.castIronLampOff), new Object[] {"ccc", "flf", " f ", 'c', ModBlocks.blockCastIron, 'f', ModBlocks.railingCastIron, 'l', ModItems.lightBulb});
 		//Items
-		GameRegistry.addShapedRecipe(new ItemStack(ModItems.lightBulb), "ppp", "p p", "pcp", 'p', Block.thinGlass, 'c', ModItems.copperWire);
+		GameRegistry.addShapedRecipe(new ItemStack(ModItems.lightBulb), new Object[] {"ppp", "p p", "pcp", 'p', Block.thinGlass, 'c', ModItems.copperWire});
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.guideBook), new Object[] {Item.book, ModItems.lightBulb});
+			//Parts
+		Random random = new Random();
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.pileParts, 1, random.nextInt(4)), new Object[] {ModItems.partsBrass, ModItems.partsBrass, ModItems.partsBrass, ModItems.partsBrass, ModItems.partsBrass, ModItems.partsBrass,ModItems.partsBrass, ModItems.partsBrass, ModItems.partsBrass});
 	}
 
 }
