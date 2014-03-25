f3e6e2834f186ef36207401f25ffdd7b478becb4
warlordjones <warlordjones@gmail.com>
2014-03-25 17:00:38 +0000

    More Recipes (And beginnings of modcompat)

---
+++ common/steamcraft/common/core/compat/IUniversallyWrenchable.java
@@ -0,0 +1,8 @@
+package common.steamcraft.common.core.compat;
+
+import ic2.api.tile.IWrenchable;
+import cofh.api.block.IDismantleable;
+
+public interface IUniversallyWrenchable extends IDismantleable, IWrenchable{
+//Just a helper class to make blocks compatible with TE and IC2 Wrenches. Will also work with BC Wrenches, if we get the API
+}
---
+++ common/steamcraft/common/core/compat/ModCompatLayer.java
@@ -0,0 +1,10 @@
+package common.steamcraft.common.core.compat;
+
+import common.steamcraft.common.core.helper.CompatHelper;
+
+public class ModCompatLayer {
+public static void loadModCompat()
+{
+	
+}
+}
