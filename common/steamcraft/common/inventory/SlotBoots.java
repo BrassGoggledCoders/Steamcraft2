58c5661a6caa2bd5600975a7541dd83fa52cab02
Arona Jones <warlordjones@gmail.com>
2014-04-06 15:37:26 +0100

    Interfaces for other slots

--- common/steamcraft/common/inventory/SlotBoots.java
+++ common/steamcraft/common/inventory/SlotBoots.java
@@ -17,6 +17,8 @@
  */
 package common.steamcraft.common.inventory;
 
+import common.steamcraft.api.vanity.IBoots;
+
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.Item;
@@ -42,7 +44,7 @@ super(inventory, par2, par3, par4);
 public boolean isItemValid(ItemStack itemstack)
 {
 
-return itemstack.getItem() instanceof Item;//instanceof ItemBoots;
+return itemstack.getItem() instanceof IBoots;
 }
 /**
 * Returns the maximum stack size for a given slot (usually the same as getInventoryStackLimit(), but 1 in the case
--- common/steamcraft/common/inventory/SlotHat.java
+++ common/steamcraft/common/inventory/SlotHat.java
@@ -21,8 +21,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
-
-import common.steamcraft.api.IHat;
+import common.steamcraft.api.vanity.IHat;
 
 /**
  * @author warlordjones
