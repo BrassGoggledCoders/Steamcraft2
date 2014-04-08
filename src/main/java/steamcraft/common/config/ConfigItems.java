e634a963125709cfdfb0be71d498b65a00af486f
Johnny Eatmon <jmaeatmon@gmail.com>
2014-04-08 11:41:15 -0400

    Item loading issues resolved

--- src/main/java/steamcraft/common/config/ConfigItems.java
+++ src/main/java/steamcraft/common/config/ConfigItems.java
@@ -21,6 +21,7 @@ import steamcraft.common.items.ItemResource;
 import steamcraft.common.items.ItemTeaSeed;
 import steamcraft.common.items.ItemWatch;
 import steamcraft.common.items.armor.ItemCustomArmor;
+import steamcraft.common.lib.LibInfo;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 /**
@@ -36,17 +37,15 @@ public class ConfigItems
     public static Item itemChisel;
     public static Item itemBrassGoggles;
 
-    public static Item coldKettle;
-    public static Item hotKettle;
-    public static Item emptyTeacup;
-    public static Item fullTeacup;
-    public static Item emptyKettle;
+    public static Item itemColdKettle;
+    public static Item itemHotKettle;
+    public static Item itemEmptyKettle;
+    public static Item itemEmptyTeacup;
+    public static Item itemFullTeacup;
 
     public static Item itemSteamCanister;
-    public static Item canisterGas;
-    public static Item steamCanister;
-    public static Item gasCanister;
-    public static Item emptyCanister;
+    public static Item itemCanisterGas;
+    public static Item itemEmptyCanister;
 
     public static void init()
 	{
@@ -58,18 +57,18 @@ public class ConfigItems
 	public static void initializeItems()
 	{
 		itemMechanical = new ItemMechanical(Config.itemMechanicalId).setUnlocalizedName("ItemMechanical");
-		GameRegistry.registerItem(itemMechanical, "ItemMechanical", "Steamcraft");
+		GameRegistry.registerItem(itemMechanical, "ItemMechanical", LibInfo.ID);
 		itemResource = new ItemResource(Config.itemResourceId).setUnlocalizedName("ItemResource");
-		GameRegistry.registerItem(itemResource, "ItemResource", "Steamcraft");
+		GameRegistry.registerItem(itemResource, "ItemResource", LibInfo.ID);
 		itemWatch = new ItemWatch(Config.itemWatchId).setUnlocalizedName("ItemWatch");
-		GameRegistry.registerItem(itemWatch, "ItemWatch", "Steamcraft");
+		GameRegistry.registerItem(itemWatch, "ItemWatch", LibInfo.ID);
 		
 		itemTeaSeed = new ItemTeaSeed(Config.itemTeaSeedId).setUnlocalizedName("ItemTeaSeed");
-		GameRegistry.registerItem(itemTeaSeed, "ItemTeaSeed", "Steamcraft");
+		GameRegistry.registerItem(itemTeaSeed, "ItemTeaSeed", LibInfo.ID);
 		itemChisel = new ItemChisel(Config.itemChiselId).setUnlocalizedName("ItemChisel");
-		GameRegistry.registerItem(itemChisel, "ItemChisel", "Steamcraft");
+		GameRegistry.registerItem(itemChisel, "ItemChisel", LibInfo.ID);
 		
 		itemBrassGoggles = new ItemCustomArmor(Config.itemGogglesId, EnumArmorMaterial.CHAIN, 0, 0).setUnlocalizedName("ItemCustomArmor");
-		GameRegistry.registerItem(itemBrassGoggles, "ItemCustomArmor", "Steamcraft");
+		GameRegistry.registerItem(itemBrassGoggles, "ItemCustomArmor", LibInfo.ID);
 	}
 }
