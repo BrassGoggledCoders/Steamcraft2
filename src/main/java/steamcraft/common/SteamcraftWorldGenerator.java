33117c5435944e7e7cad827e2fd9d78d7413a3e2
Arona Jones <warlordjones@gmail.com>
2014-04-09 09:16:14 +0100

    Generation Tweaks. Still doesn't work...

--- src/main/java/steamcraft/common/SteamcraftWorldGenerator.java
+++ src/main/java/steamcraft/common/SteamcraftWorldGenerator.java
@@ -6,12 +6,13 @@ import java.util.Random;
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
 import steamcraft.common.config.Config;
 import steamcraft.common.config.ConfigBlocks;
 import cpw.mods.fml.common.IWorldGenerator;
 
 /**
- * @author MrArcane111
+ * @author Surseance
  *
  */
 public class SteamcraftWorldGenerator implements IWorldGenerator {
@@ -44,7 +45,7 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int borniteYCoord = random.nextInt(Config.borniteHeight);
 			int borniteZCoord = chunkZ + random.nextInt(16);
 
-			new SmartOreGeneration(ConfigBlocks.blockCustomOre.blockID, 0, random.nextInt(3), Block.stone.blockID).generate(world, random, borniteXCoord, borniteYCoord, borniteZCoord);
+			new WorldGenMinable(ConfigBlocks.blockCustomOre.blockID, 0, random.nextInt(3), Block.stone.blockID).generate(world, random, borniteXCoord, borniteYCoord, borniteZCoord);
 		}
 		if(Config.brimstoneOreGenEnabled)
 		{
@@ -53,7 +54,7 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int brimstoneYCoord = random.nextInt(Config.brimstoneHeight);
 			int brimstoneZCoord = chunkZ + random.nextInt(16);
 
-			new SmartOreGeneration(ConfigBlocks.blockCustomOre.blockID, 1, random.nextInt(3), Block.stone.blockID).generate(world, random, brimstoneXCoord, brimstoneYCoord, brimstoneZCoord);
+			new WorldGenMinable(ConfigBlocks.blockCustomOre.blockID, 1, random.nextInt(3), Block.stone.blockID).generate(world, random, brimstoneXCoord, brimstoneYCoord, brimstoneZCoord);
 		}
 		}
 		}
@@ -64,7 +65,7 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int zincYCoord = random.nextInt(Config.phosphateHeight);
 			int zincZCoord = chunkZ + random.nextInt(16);
 
-			new SmartOreGeneration(ConfigBlocks.blockCustomOre.blockID, 2, random.nextInt(3), Block.stone.blockID).generate(world, random, zincXCoord, zincYCoord, zincZCoord);
+			new WorldGenMinable(ConfigBlocks.blockCustomOre.blockID, 2, random.nextInt(3), Block.stone.blockID).generate(world, random, zincXCoord, zincYCoord, zincZCoord);
 		}
 		if(Config.uraniumOreGenEnabled)
 		{
@@ -73,7 +74,7 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int uraniteYCoord = random.nextInt(Config.uraniumHeight);
 			int uraniteZCoord = chunkZ + random.nextInt(16);
 
-			new SmartOreGeneration(ConfigBlocks.blockCustomOre.blockID, 3, random.nextInt(3), Block.stone.blockID).generate(world, random, uraniteXCoord, uraniteYCoord, uraniteZCoord);
+			new WorldGenMinable(ConfigBlocks.blockCustomOre.blockID, 3, random.nextInt(3), Block.stone.blockID).generate(world, random, uraniteXCoord, uraniteYCoord, uraniteZCoord);
 		}
 		}
 		}
@@ -84,7 +85,7 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int aluminumYCoord = random.nextInt(Config.aluminumHeight);
 			int aluminumZCoord = chunkZ + random.nextInt(16);
 
-			new SmartOreGeneration(ConfigBlocks.blockCustomOre.blockID, 4, random.nextInt(3), Block.stone.blockID).generate(world, random, aluminumXCoord, aluminumYCoord, aluminumZCoord);
+			new WorldGenMinable(ConfigBlocks.blockCustomOre.blockID, 4, random.nextInt(3), Block.stone.blockID).generate(world, random, aluminumXCoord, aluminumYCoord, aluminumZCoord);
 		}
 		}
 		if(Config.copperOreGenEnabled)
@@ -94,7 +95,7 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int copperYCoord = random.nextInt(Config.copperHeight);
 			int copperZCoord = chunkZ + random.nextInt(16);
 
-			new SmartOreGeneration(ConfigBlocks.blockCustomOre.blockID, 5, random.nextInt(3), Block.stone.blockID).generate(world, random, copperXCoord, copperYCoord, copperZCoord);
+			new WorldGenMinable(ConfigBlocks.blockCustomOre.blockID, 5, random.nextInt(3), Block.stone.blockID).generate(world, random, copperXCoord, copperYCoord, copperZCoord);
 		}
 		}
 		if(Config.tinOreGenEnabled)
@@ -104,7 +105,7 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int tinYCoord = random.nextInt(Config.tinHeight);
 			int tinZCoord = chunkZ + random.nextInt(16);
 
-			new SmartOreGeneration(ConfigBlocks.blockCustomOre.blockID, 6, random.nextInt(3), Block.stone.blockID).generate(world, random, tinXCoord, tinYCoord, tinZCoord);
+			new WorldGenMinable(ConfigBlocks.blockCustomOre.blockID, 6, random.nextInt(3), Block.stone.blockID).generate(world, random, tinXCoord, tinYCoord, tinZCoord);
 		}
 		}
 		if(Config.zincOreGenEnabled)
@@ -114,7 +115,7 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int zincYCoord = random.nextInt(Config.zincHeight);
 			int zincZCoord = chunkZ + random.nextInt(16);
 
-			new SmartOreGeneration(ConfigBlocks.blockCustomOre.blockID, 7, random.nextInt(3), Block.stone.blockID).generate(world, random, zincXCoord, zincYCoord, zincZCoord);
+			new WorldGenMinable(ConfigBlocks.blockCustomOre.blockID, 7, random.nextInt(3), Block.stone.blockID).generate(world, random, zincXCoord, zincYCoord, zincZCoord);
 		}
 		}
 		if(Config.etheriumOreGenEnabled)
@@ -124,7 +125,7 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int etheriumYCoord = 8 + random.nextInt(Config.etheriumHeight);
 			int etheriumZCoord = chunkZ + random.nextInt(16);
 
-			//new SmartOreGeneration(ConfigBlocks.blockCustomOre.blockID, 8, random.nextInt(3), Block.stone.blockID).generate(world, random, etheriumXCoord, etheriumYCoord, etheriumZCoord);
+			//new WorldGenMinable(ConfigBlocks.blockCustomOre.blockID, 8, random.nextInt(3), Block.stone.blockID).generate(world, random, etheriumXCoord, etheriumYCoord, etheriumZCoord);
 		}
 		}
 	}
@@ -136,8 +137,8 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int etheriumXCoord = chunkX + random.nextInt(16);
 			int etheriumYCoord = random.nextInt(Config.netherEtheriumCrystalHeight);
 			int etheriumZCoord = chunkZ + random.nextInt(16);
-
-			new SmartOreGeneration(ConfigBlocks.blockCrystal.blockID, 0, random.nextInt(3), Block.stone.blockID).generate(world, random, etheriumXCoord, etheriumYCoord, etheriumZCoord);
+			//TODO: FIX THIS
+			//new WorldGenMinable(ConfigBlocks.blockCrystal.blockID, 0, random.nextInt(3), Block.stone.blockID).generate(world, random, etheriumXCoord, etheriumYCoord, etheriumZCoord);
 		}
 		}
 	}
@@ -149,8 +150,8 @@ public class SteamcraftWorldGenerator implements IWorldGenerator {
 			int phosphateXCoord = chunkX + random.nextInt(16);
 			int phosphateYCoord = random.nextInt(Config.endEtheriumCrystalHeight);
 			int phosphateZCoord = chunkZ + random.nextInt(16);
-
-			new SmartOreGeneration(ConfigBlocks.blockCrystal.blockID, 0, random.nextInt(3), Block.stone.blockID).generate(world, random, phosphateXCoord, phosphateYCoord, phosphateZCoord);
+			//TODO: FIX THIS
+			//new WorldGenMinable(ConfigBlocks.blockCrystal.blockID, 0, random.nextInt(3), Block.stone.blockID).generate(world, random, phosphateXCoord, phosphateYCoord, phosphateZCoord);
 		}
 		}
 	}
