2e6744cc509a2f3054789c29ead3096b94383794
Johnny Eatmon <jmaeatmon@gmail.com>
2014-04-11 19:21:00 -0400

    Ta-da!
    Back in business

--- common/steamcraft/common/lib/EntityIDs.java
+++
@@ -1,9 +0,0 @@
-package common.steamcraft.common.lib2;
-
-import common.steamcraft.common.SC2;
-
-public class EntityIDs
-{
-	public static final int BULLET_ID = SC2.proxy.cfgHandler.loadEntity("BULLET_ID");
-	public static final int LIGHTNINGROD_ID = SC2.proxy.cfgHandler.loadEntity("LIGHTNINGROD_ID");
-}
