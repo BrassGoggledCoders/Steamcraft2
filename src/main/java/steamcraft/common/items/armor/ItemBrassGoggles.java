4aa78f1c8179c2979b3c1dda81054b9b82f16dba
Arona Jones <warlordjones@gmail.com>
2014-06-29 21:15:04 +0100

    More Efficient Goggles overlay handling

--- src/main/java/steamcraft/common/items/armor/ItemBrassGoggles.java
+++ src/main/java/steamcraft/common/items/armor/ItemBrassGoggles.java
@@ -3,9 +3,18 @@
  */
 package steamcraft.common.items.armor;
 
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import steamcraft.common.config.ConfigItems;
 import steamcraft.common.lib.LibInfo;
+import steamcraft.common.lib.events.EventHandlerForge;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -18,7 +27,7 @@ public class ItemBrassGoggles extends BaseArmor
 
 	/**
 	 * Instantiates a new item brass goggles.
-	 * 
+	 *
 	 * @param mat
 	 *            the mat
 	 * @param p_i45325_2_
@@ -38,5 +47,53 @@ public class ItemBrassGoggles extends BaseArmor
 	public String getArmorTexture(ItemStack is, Entity entity, int slot, String type)
 	{
 		return LibInfo.PREFIX + "textures/armor/goggles.png";
+	}
+	/** The overlay. */
+	private static ResourceLocation overlay = new ResourceLocation(LibInfo.PREFIX + "textures/misc/goggles.png");
+
+	@SideOnly(Side.CLIENT)
+	public void renderHelmetOverlay(ItemStack stack, EntityPlayer player, ScaledResolution resolution, float partialTicks, boolean hasScreen, int mouseX, int mouseY)
+	{
+		if ((Minecraft.getMinecraft().thePlayer == null) || (Minecraft.getMinecraft().currentScreen != null))
+			return;
+
+		ItemStack helmet = Minecraft.getMinecraft().thePlayer.inventory.armorItemInSlot(3);
+
+		if ((Minecraft.getMinecraft().gameSettings.thirdPersonView == 0) && (helmet != null) && (helmet.getItem() == ConfigItems.brassGoggles))// &&
+		// KeyHandler.keyPressed)
+		{
+			Minecraft.getMinecraft().getTextureManager().bindTexture(overlay);
+			Tessellator tessellator = Tessellator.instance;
+			ScaledResolution scaledResolution = new ScaledResolution(Minecraft.getMinecraft().gameSettings,
+					Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
+			int width = scaledResolution.getScaledWidth();
+			int height = scaledResolution.getScaledHeight();
+
+			GL11.glDisable(GL11.GL_DEPTH_TEST);
+			GL11.glDepthMask(false);
+			// GL11.glEnable(GL11.GL_BLEND);
+			// GL11.glBlendFunc(GL11.GL_DST_COLOR, GL11.GL_SRC_COLOR);
+			// GL11.glColor3f(1.0F, 1.0F, 1.0F);
+			GL11.glDisable(GL11.GL_ALPHA_TEST);
+			GL11.glClearDepth(1.0D);
+			tessellator.startDrawingQuads();
+			tessellator.addVertexWithUV(0.0D, height, 90.0D, 0.0D, 1.0D);
+			tessellator.addVertexWithUV(width, height, 90.0D, 1.0D, 1.0D);
+			tessellator.addVertexWithUV(width, 0.0D, 90.0D, 1.0D, 0.0D);
+			tessellator.addVertexWithUV(0.0D, 0.0D, 90.0D, 0.0D, 0.0D);
+			tessellator.draw();
+			GL11.glDepthMask(true);
+			GL11.glEnable(GL11.GL_DEPTH_TEST);
+			GL11.glEnable(GL11.GL_ALPHA_TEST);
+			GL11.glDisable(GL11.GL_BLEND);
+
+			/*
+			 * if (!mc.gameSettings.hideGUI || mc.currentScreen != null) {
+			 * int x = (Mouse.getX() * width) / mc.displayWidth; int y =
+			 * height - (Mouse.getY() * height) / mc.displayHeight - 1;
+			 * mc.ingameGUI.renderGameOverlay(0.0F, mc.currentScreen !=
+			 * null, x, y); }
+			 */
+		}
 	}
 }
