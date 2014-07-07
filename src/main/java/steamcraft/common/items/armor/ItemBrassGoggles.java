2d2200b9eb5d16b6897de6075694f68ab6098211
Arona Jones <warlordjones@gmail.com>
2014-07-07 23:30:01 +0100

    Properly reimplement goggles things from DrawHighlight

--- src/main/java/steamcraft/common/items/armor/ItemBrassGoggles.java
+++ src/main/java/steamcraft/common/items/armor/ItemBrassGoggles.java
@@ -1,5 +1,5 @@
 /**
- * This class was created by BrassGoggledCoders modding team. 
+ * This class was created by BrassGoggledCoders modding team.
  * This class is available as part of the Steamcraft 2 Mod for Minecraft.
  *
  * Steamcraft 2 is open-source and is distributed under the MMPL v1.0 License.
@@ -8,7 +8,7 @@
  * Steamcraft 2 is based on the original Steamcraft Mod created by Proloe.
  * Steamcraft (c) Proloe 2011
  * (http://www.minecraftforum.net/topic/251532-181-steamcraft-source-code-releasedmlv054wip/)
- * 
+ *
  */
 package steamcraft.common.items.armor;
 
@@ -34,7 +34,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ItemBrassGoggles extends BaseArmor
 {
 	private static ResourceLocation overlay = new ResourceLocation(LibInfo.PREFIX + "textures/misc/goggles.png");
-	
+
 	public ItemBrassGoggles(ArmorMaterial mat, int renderIndex, int type)
 	{
 		super(mat, renderIndex, type);
@@ -44,7 +44,7 @@ public class ItemBrassGoggles extends BaseArmor
 	@SideOnly(Side.CLIENT)
 	public String getArmorTexture(ItemStack is, Entity entity, int slot, String type)
 	{
-		return LibInfo.PREFIX + "textures/armor/divinghelmet.png";
+		return LibInfo.PREFIX + "textures/armor/goggles.png";
 	}
 
 	@Override
