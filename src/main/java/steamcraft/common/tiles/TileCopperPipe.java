93e1734f57b8470a87172609d7223abc6252bcf1
Decebaldecebal <rares_serban96@yahoo.com>
2014-07-10 13:41:38 +0300

    Fixed extracting syncing issue.NBT saving has some problems...

--- src/main/java/steamcraft/common/tiles/TileCopperPipe.java
+++ src/main/java/steamcraft/common/tiles/TileCopperPipe.java
@@ -16,6 +16,9 @@ import java.util.ArrayList;
 
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
@@ -117,6 +120,22 @@ public class TileCopperPipe extends TileEntity implements IFluidHandler
 		return dir;
 	}
 
+	@Override
+	public Packet getDescriptionPacket()
+	{
+		NBTTagCompound tag = new NBTTagCompound();
+		
+		writeDirectionToNBT(tag, extract);
+		
+		return new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 1, tag);
+	}
+	
+	@Override
+	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity packet)
+	{
+		extract = readDirectionFromNBT(packet.func_148857_g());
+	}
+	
 	public void changeExtracting()
 	{
 		if(extract!=null)
