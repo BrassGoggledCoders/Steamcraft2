99789a8e8a739d7ef108060859b9eb8bbdb06883
Arona Jones <warlordjones@gmail.com>
2014-07-16 13:10:03 +0200

    Name for bullet damage source

--- src/main/java/steamcraft/common/lib/DamageSourceHandler.java
+++ src/main/java/steamcraft/common/lib/DamageSourceHandler.java
@@ -21,5 +21,5 @@ import net.minecraft.util.DamageSource;
 public class DamageSourceHandler
 {
 	public static DamageSource electrocution = new DamageSource(LibInfo.ID + ".electrocution").setDamageBypassesArmor().setDifficultyScaled();
-	public static DamageSource bullet = new DamageSource(LibInfo.ID + ".bullet").setDamageBypassesArmor();
+	public static DamageSource bullet = new DamageSource(LibInfo.ID + ".bullet").setDamageBypassesArmor().setProjectile();
 }
