07baf59c5debe710ab34aa687686794ae9b5d84b
Arona Jones <warlordjones@gmail.com>
2014-07-28 10:52:02 +0100

    Version bump

--- src/main/java/steamcraft/common/lib/LibInfo.java
+++ src/main/java/steamcraft/common/lib/LibInfo.java
@@ -1,5 +1,5 @@
 /**
- * This class was created by BrassGoggledCoders modding team. 
+ * This class was created by BrassGoggledCoders modding team.
  * This class is available as part of the Steamcraft 2 Mod for Minecraft.
  *
  * Steamcraft 2 is open-source and is distributed under the MMPL v1.0 License.
@@ -8,7 +8,7 @@
  * Steamcraft 2 is based on the original Steamcraft Mod created by Proloe.
  * Steamcraft (c) Proloe 2011
  * (http://www.minecraftforum.net/topic/251532-181-steamcraft-source-code-releasedmlv054wip/)
- * 
+ *
  */
 package steamcraft.common.lib;
 
@@ -17,15 +17,15 @@ import java.util.List;
 
 /**
  * A neat, tidy class for holding various bits of mod information.
- * 
+ *
  * @author Surseance
- * 
+ *
  */
 public class LibInfo
 {
 	public static final String ID = "steamcraft2";
 	public static final String NAME = "Steamcraft 2";
-	public static final String VERSION = "Beta R1.2";
+	public static final String VERSION = "Beta R2";
 
 	public static final List<String> AUTHORS = Arrays.asList("warlordjones", "decebaldecebal", "Falkok15", "wierdude1999", "seanvanpelt2 & others");
 
