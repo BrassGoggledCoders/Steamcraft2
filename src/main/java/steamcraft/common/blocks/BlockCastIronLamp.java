e850e739ea02de4e811e4ee6c2b9daf22289cedc
Arona Jones <warlordjones@gmail.com>
2015-01-17 16:58:37 +0000

    Stop the lamp losing meta when powered

--- src/main/java/steamcraft/common/blocks/BlockCastIronLamp.java
+++ src/main/java/steamcraft/common/blocks/BlockCastIronLamp.java
@@ -120,26 +120,30 @@ public class BlockCastIronLamp extends BlockContainer
 	public void onBlockAdded(World world, int x, int y, int z)
 	{
 		if(!world.isRemote)
+		{
 			if(this.powered && !world.isBlockIndirectlyGettingPowered(x, y, z))
 			{
 				world.scheduleBlockUpdate(x, y, z, this, 4);
-				world.setBlock(x, y, z, InitBlocks.blockCastIronLamp);
+				world.setBlock(x, y, z, InitBlocks.blockCastIronLamp, world.getBlockMetadata(x, y, z), 2);
 			}
 			else if(!this.powered && world.isBlockIndirectlyGettingPowered(x, y, z))
-				world.setBlock(x, y, z, InitBlocks.blockCastIronLampOn);
+				world.setBlock(x, y, z, InitBlocks.blockCastIronLampOn, world.getBlockMetadata(x, y, z), 2);
+		}
 	}
 
 	@Override
 	public void onNeighborBlockChange(World world, int x, int y, int z, Block p_149695_5_)
 	{
 		if(!world.isRemote)
+		{
 			if(this.powered && !world.isBlockIndirectlyGettingPowered(x, y, z))
 			{
 				world.scheduleBlockUpdate(x, y, z, this, 4);
-				world.setBlock(x, y, z, InitBlocks.blockCastIronLamp);
+				world.setBlock(x, y, z, InitBlocks.blockCastIronLamp, world.getBlockMetadata(x, y, z), 2);
 			}
 			else if(!this.powered && world.isBlockIndirectlyGettingPowered(x, y, z))
-				world.setBlock(x, y, z, InitBlocks.blockCastIronLampOn);
+				world.setBlock(x, y, z, InitBlocks.blockCastIronLampOn, world.getBlockMetadata(x, y, z), 2);
+		}
 	}
 
 	@Override
