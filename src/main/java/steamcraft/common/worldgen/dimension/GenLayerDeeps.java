7d02104d8c36094f08b92d6bfc457ebc63c4dc4f
Arona Jones <warlordjones@gmail.com>
2015-01-19 21:27:02 +0000

    More GenLayer stuff

--- src/main/java/steamcraft/common/worldgen/dimension/GenLayerDeeps.java
+++ src/main/java/steamcraft/common/worldgen/dimension/GenLayerDeeps.java
@@ -1,11 +1,8 @@
 package steamcraft.common.worldgen.dimension;
 
 import net.minecraft.world.gen.layer.GenLayer;
-import net.minecraft.world.gen.layer.GenLayerAddIsland;
 import net.minecraft.world.gen.layer.GenLayerFuzzyZoom;
 import net.minecraft.world.gen.layer.GenLayerIsland;
-import net.minecraft.world.gen.layer.GenLayerRemoveTooMuchOcean;
-import net.minecraft.world.gen.layer.GenLayerSmooth;
 import net.minecraft.world.gen.layer.GenLayerVoronoiZoom;
 import net.minecraft.world.gen.layer.GenLayerZoom;
 
@@ -18,26 +15,20 @@ public abstract class GenLayerDeeps extends GenLayer
 
 	public static GenLayer[] makeTheWorld(long seed)
 	{
-		GenLayer biomes = new GenLayerBiomesDeeps(1L);
+		GenLayer biomes = new GenLayerIsland(1L);
+		biomes = new GenLayerFuzzyZoom(2000L, biomes);
 
-		biomes = new GenLayerZoom(1000L, biomes);
-		biomes = new GenLayerZoom(1001L, biomes);
-		biomes = new GenLayerZoom(1002L, biomes);
-		biomes = new GenLayerZoom(1003L, biomes);
-		biomes = new GenLayerZoom(1004L, biomes);
-		biomes = new GenLayerZoom(1005L, biomes);
+		biomes = new GenLayerBiomesDeeps(100L, biomes);
+		biomes = GenLayerZoom.magnify(2000L, biomes, 1);
 
-		GenLayerIsland genlayerisland = new GenLayerIsland(1L);
-		GenLayerFuzzyZoom genlayerfuzzyzoom = new GenLayerFuzzyZoom(2000L, genlayerisland);
-		GenLayerAddIsland genlayeraddisland = new GenLayerAddIsland(1L, genlayerfuzzyzoom);
-		GenLayerZoom genlayerzoom = new GenLayerZoom(2001L, genlayeraddisland);
-		genlayeraddisland = new GenLayerAddIsland(2L, genlayerzoom);
-		genlayeraddisland = new GenLayerAddIsland(50L, genlayeraddisland);
-		genlayeraddisland = new GenLayerAddIsland(70L, genlayeraddisland);
-		GenLayer shore = new GenLayerShore(1000L, biomes);
-		GenLayer smooth = new GenLayerSmooth(1000L, shore);
-		GenLayer smooth1 = new GenLayerSmooth(1000L, smooth);
-		GenLayerRemoveTooMuchOcean genlayerremovetoomuchocean = new GenLayerRemoveTooMuchOcean(2L, smooth1);
+		/*
+		 * TODO GenLayerIsland genlayerisland = new GenLayerIsland(1L); GenLayerFuzzyZoom genlayerfuzzyzoom = new GenLayerFuzzyZoom(2000L, genlayerisland);
+		 * GenLayerAddIsland genlayeraddisland = new GenLayerAddIsland(1L, genlayerfuzzyzoom); GenLayerZoom genlayerzoom = new GenLayerZoom(2001L,
+		 * genlayeraddisland); genlayeraddisland = new GenLayerAddIsland(2L, genlayerzoom); genlayeraddisland = new GenLayerAddIsland(50L, genlayeraddisland);
+		 * genlayeraddisland = new GenLayerAddIsland(70L, genlayeraddisland); GenLayer shore = new GenLayerShore(1000L, biomes); GenLayer smooth = new
+		 * GenLayerSmooth(1000L, shore); GenLayer smooth1 = new GenLayerSmooth(1000L, smooth); GenLayerRemoveTooMuchOcean genlayerremovetoomuchocean = new
+		 * GenLayerRemoveTooMuchOcean(2L, smooth1);
+		 */
 
 		GenLayer genlayervoronoizoom = new GenLayerVoronoiZoom(10L, biomes);
 		biomes.initWorldGenSeed(seed);
