fcdbd8aa3ca9d6e8b1696b0ebe68f263fd87bf44
Arona Jones <warlordjones@gmail.com>
2015-01-27 20:05:01 +0000

    Move donor list to boilerplate

--- src/main/java/steamcraft/common/lib/LibInfo.java
+++ src/main/java/steamcraft/common/lib/LibInfo.java
@@ -1,5 +1,6 @@
 package steamcraft.common.lib;
 
+import boilerplate.common.Boilerplate;
 import boilerplate.common.utils.StringUtils;
 
 public class LibInfo
@@ -10,7 +11,6 @@ public class LibInfo
 			"Ryan_T15", "AdmiralSocrates" };
 	public static String[] devs = { "warlordjones", "decebaldecebal", "Snurly" };
 	public static String[] contributors = { "domi1819", "frothiny", "MrIbby", "HCSarise" };
-	public static String[] donors = {};
 	public static String[] otherCoolPeople = { "ClockwerkKaiser" };
-	public static String[] names = StringUtils.merge(brassBlocksMembers, devs, contributors, donors, otherCoolPeople);
+	public static String[] names = StringUtils.merge(brassBlocksMembers, devs, contributors, Boilerplate.donors, otherCoolPeople);
 }
