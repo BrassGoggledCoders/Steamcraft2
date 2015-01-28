8020356d407324d37db3710af2582bf64af3ec63
Arona Jones <warlordjones@gmail.com>
2015-01-28 10:54:56 +0000

    Insert file headers

--- src/main/java/steamcraft/common/worldgen/WorldGenLeafPile.java
+++ src/main/java/steamcraft/common/worldgen/WorldGenLeafPile.java
@@ -1,3 +1,15 @@
+/**
+ * This class was created by BrassGoggledCoders modding team. 
+ * This class is available as part of the Steamcraft 2 Mod for Minecraft.
+ *
+ * Steamcraft 2 is open-source and is distributed under the MMPL v1.0 License.
+ * (http://www.mod-buildcraft.com/MMPL-1.0.txt)
+ *
+ * Steamcraft 2 is based on the original Steamcraft Mod created by Proloe.
+ * Steamcraft (c) Proloe 2011
+ * (http://www.minecraftforum.net/topic/251532-181-steamcraft-source-code-releasedmlv054wip/)
+ * 
+ */
 package steamcraft.common.worldgen;
 
 import java.util.Random;
@@ -15,11 +27,11 @@ public class WorldGenLeafPile extends WorldGenerator
 	{
 		for(int l = 0; l < 64; ++l)
 		{
-			int i1 = p_76484_3_ + p_76484_2_.nextInt(8) - p_76484_2_.nextInt(8);
-			int j1 = p_76484_4_ + p_76484_2_.nextInt(4) - p_76484_2_.nextInt(4);
-			int k1 = p_76484_5_ + p_76484_2_.nextInt(8) - p_76484_2_.nextInt(8);
+			int i1 = (p_76484_3_ + p_76484_2_.nextInt(8)) - p_76484_2_.nextInt(8);
+			int j1 = (p_76484_4_ + p_76484_2_.nextInt(4)) - p_76484_2_.nextInt(4);
+			int k1 = (p_76484_5_ + p_76484_2_.nextInt(8)) - p_76484_2_.nextInt(8);
 
-			if(p_76484_1_.isAirBlock(i1, j1, k1) && p_76484_1_.getBlock(i1, j1 - 1, k1) == Blocks.grass
+			if(p_76484_1_.isAirBlock(i1, j1, k1) && (p_76484_1_.getBlock(i1, j1 - 1, k1) == Blocks.grass)
 					&& InitBlocks.blockLeafCover.canPlaceBlockAt(p_76484_1_, i1, j1, k1))
 			{
 				p_76484_1_.setBlock(i1, j1, k1, InitBlocks.blockLeafCover, p_76484_2_.nextInt(4), 2);
