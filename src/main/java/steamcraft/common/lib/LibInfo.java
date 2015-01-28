5e4df83340539f1067cc298e5253beb3aabee5ce
Arona Jones <warlordjones@gmail.com>
2015-01-28 10:58:55 +0000

    Move dev list to Boilerplate

--- src/main/java/steamcraft/common/lib/LibInfo.java
+++ src/main/java/steamcraft/common/lib/LibInfo.java
@@ -1,5 +1,5 @@
 /**
- * This class was created by BrassGoggledCoders modding team. 
+ * This class was created by BrassGoggledCoders modding team.
  * This class is available as part of the Steamcraft 2 Mod for Minecraft.
  *
  * Steamcraft 2 is open-source and is distributed under the MMPL v1.0 License.
@@ -8,7 +8,7 @@
  * Steamcraft 2 is based on the original Steamcraft Mod created by Proloe.
  * Steamcraft (c) Proloe 2011
  * (http://www.minecraftforum.net/topic/251532-181-steamcraft-source-code-releasedmlv054wip/)
- * 
+ *
  */
 package steamcraft.common.lib;
 
@@ -21,10 +21,9 @@ public class LibInfo
 	public static String[] metalsV = { "Iron", "Gold" };
 	private static String[] brassBlocksMembers = { "Longeye", "DocArcane", "CaptianNivea", "DrVesuvius", "PeaGreen", "elShoggotho", "Ainuryn", "Piyok0",
 			"Ryan_T15", "AdmiralSocrates" };
-	private static String[] devs = { "warlordjones", "decebaldecebal", "Snurly" };
-	private static String[] contributors = { "domi1819", "frothiny", "MrIbby", "HCSarise" };
+	public static String[] contributors = { "domi1819", "frothiny", "MrIbby", "HCSarise" };
 	private static String[] otherCoolPeople = { "ClockwerkKaiser" };
-	public static String[] names = StringUtils.merge(brassBlocksMembers, devs, contributors, Boilerplate.donors, otherCoolPeople);
+	public static String[] names = StringUtils.merge(brassBlocksMembers, Boilerplate.devs, contributors, Boilerplate.donors, otherCoolPeople);
 	public static float pixel = 1F / 16F / 2F;
 	public static float tPixel = 1F / 32F;
 }
