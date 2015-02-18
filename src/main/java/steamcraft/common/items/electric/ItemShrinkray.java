fff758a24ee42a6dd186cfa1091b8b6a6820f42c
Arona Jones <warlordjones@gmail.com>
2015-02-18 19:19:04 +0000

    Safety Catch for the Shrinkray

--- src/main/java/steamcraft/common/items/electric/ItemShrinkray.java
+++ src/main/java/steamcraft/common/items/electric/ItemShrinkray.java
@@ -14,9 +14,11 @@ package steamcraft.common.items.electric;
 
 import java.awt.Color;
 import java.util.HashMap;
+import java.util.List;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
@@ -51,65 +53,97 @@ public class ItemShrinkray extends ElectricItem
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
 	{
-		if(this.getEnergyStored(stack) > ItemShrinkray.energyPerUse)
+		if(player.isSneaking())
 		{
-			MovingObjectPosition mop = PlayerUtils.getTargetBlock(world, player, true, 10);
+			NBTTagCompound tag = stack.getTagCompound();
 
-			Vec3 vec3 = player.getLookVec();
-			double tx = player.posX + (vec3.xCoord * 10.0D);
-			double ty = player.posY + (vec3.yCoord * 10.0D);
-			double tz = player.posZ + (vec3.zCoord * 10.0D);
-			int impact = 0;
-
-			if(mop != null) // Sets vector
-			{
-				tx = mop.hitVec.xCoord;
-				ty = mop.hitVec.yCoord;
-				tz = mop.hitVec.zCoord;
-				impact = 5;
-			}
-
-			// For RAY GUN sounds
-			if(soundDelay.get(player) == null)
-				soundDelay.put(player.getCommandSenderName(), Long.valueOf(0L));
-			if(!world.isRemote && (soundDelay.get(player.getCommandSenderName()).longValue() < System.currentTimeMillis()))
-			{
-				world.playSoundEffect(tx, ty, tz, ModInfo.PREFIX + "raygun", 0.35F, 1.0F);
-				soundDelay.put(player.getCommandSenderName(), Long.valueOf(System.currentTimeMillis() + 1200L));
-			}
+			if(tag.getBoolean("active"))
+				tag.setBoolean("active", false);
 			else
-				soundDelay.put(player.getCommandSenderName(), Long.valueOf(0L));
-			if(world.isRemote)
-				ray.put(player.getCommandSenderName(),
-						Steamcraft.proxy.rayFX(world, player, tx, ty, tz, 2, false, impact > 0 ? 2.0F : 0.0F, ray.get(player), impact, Color.BLUE));
-			/*
-			 * if(mop != null && mop.typeOfHit == MovingObjectPosition.MovingObjectType.ENTITY) { if(!(mop.entityHit instanceof EntityEnderman)) {
-			 * mop.entityHit.setFire(500); extractEnergy(stack, energyPerUse, false); } }
-			 */
-			if((mop != null) && (mop.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK))
-			{
-				int x = mop.blockX;
-				int y = mop.blockY;
-				int z = mop.blockZ;
-				// this.spawnParticles(world, x, y, z);
+				tag.setBoolean("active", true);
 
-				if(!world.isAirBlock(x, y, z) && !Utils.getBlockUnbreakable(world, x, y, z))
+			stack.setTagCompound(tag);
+		}
+		if(stack.getTagCompound().getBoolean("active"))
+		{
+			if(this.getEnergyStored(stack) > ItemShrinkray.energyPerUse)
+			{
 				{
-					player.worldObj.spawnEntityInWorld(new EntityMinedBlock(player.worldObj, x + 0.5F, y + 0.5F, z + 0.5F, world.getBlock(x, y, z),
-							world.getBlockMetadata(x, y, z), false));
-					// ArrayList<ItemStack> items = world.getBlock(x, y,
-					// z).getDrops(world, x, y, z, 0, 0);
-					// for(ItemStack drops : items)
-					// world.spawnEntityInWorld(new EntityItem(world, x, y, z,
-					// drops));
-					if(!world.isRemote)
+					MovingObjectPosition mop = PlayerUtils.getTargetBlock(world, player, true, 10);
+
+					Vec3 vec3 = player.getLookVec();
+					double tx = player.posX + (vec3.xCoord * 10.0D);
+					double ty = player.posY + (vec3.yCoord * 10.0D);
+					double tz = player.posZ + (vec3.zCoord * 10.0D);
+					int impact = 0;
+
+					if(mop != null) // Sets vector
 					{
-						world.setBlockToAir(x, y, z);
-						this.extractEnergy(stack, ItemShrinkray.energyPerUse, false);
+						tx = mop.hitVec.xCoord;
+						ty = mop.hitVec.yCoord;
+						tz = mop.hitVec.zCoord;
+						impact = 5;
+					}
+
+					// For RAY GUN sounds
+					if(soundDelay.get(player) == null)
+						soundDelay.put(player.getCommandSenderName(), Long.valueOf(0L));
+					if(!world.isRemote && (soundDelay.get(player.getCommandSenderName()).longValue() < System.currentTimeMillis()))
+					{
+						world.playSoundEffect(tx, ty, tz, ModInfo.PREFIX + "raygun", 0.35F, 1.0F);
+						soundDelay.put(player.getCommandSenderName(), Long.valueOf(System.currentTimeMillis() + 1200L));
+					}
+					else
+						soundDelay.put(player.getCommandSenderName(), Long.valueOf(0L));
+					if(world.isRemote)
+						ray.put(player.getCommandSenderName(),
+								Steamcraft.proxy.rayFX(world, player, tx, ty, tz, 2, false, impact > 0 ? 2.0F : 0.0F, ray.get(player), impact, Color.BLUE));
+					/*
+					 * if(mop != null && mop.typeOfHit == MovingObjectPosition.MovingObjectType.ENTITY) { if(!(mop.entityHit instanceof EntityEnderman)) {
+					 * mop.entityHit.setFire(500); extractEnergy(stack, energyPerUse, false); } }
+					 */
+					if((mop != null) && (mop.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK))
+					{
+						int x = mop.blockX;
+						int y = mop.blockY;
+						int z = mop.blockZ;
+						// this.spawnParticles(world, x, y, z);
+
+						if(!world.isAirBlock(x, y, z) && !Utils.getBlockUnbreakable(world, x, y, z))
+						{
+							player.worldObj.spawnEntityInWorld(new EntityMinedBlock(player.worldObj, x + 0.5F, y + 0.5F, z + 0.5F, world.getBlock(x, y, z),
+									world.getBlockMetadata(x, y, z), false));
+							// ArrayList<ItemStack> items = world.getBlock(x, y,
+							// z).getDrops(world, x, y, z, 0, 0);
+							// for(ItemStack drops : items)
+							// world.spawnEntityInWorld(new EntityItem(world, x, y, z,
+							// drops));
+							if(!world.isRemote)
+							{
+								world.setBlockToAir(x, y, z);
+								this.extractEnergy(stack, ItemShrinkray.energyPerUse, false);
+							}
+						}
 					}
 				}
 			}
 		}
 		return stack;
+	}
+
+	@SuppressWarnings("all")
+	@Override
+	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean bool)
+	{
+		super.addInformation(itemStack, entityPlayer, list, bool);
+
+		if(itemStack.getTagCompound().getBoolean("active"))
+		{
+			list.add("\u00A74" + "Safety Catch Off");
+		}
+		else
+		{
+			list.add("\u00A72" + "Safety Catch On");
+		}
 	}
 }
