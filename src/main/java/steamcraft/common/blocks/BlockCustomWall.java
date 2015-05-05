8c04201e4787dc6d8e38ae9aa931c352aae47f0e
Arona Jones <warlordjones@gmail.com>
2015-05-05 22:41:46 +0100

    Prevent the second metadata on wall blocks.

--- src/main/java/steamcraft/common/blocks/BlockCustomWall.java
+++ src/main/java/steamcraft/common/blocks/BlockCustomWall.java
@@ -12,8 +12,13 @@
  */
 package steamcraft.common.blocks;
 
+import java.util.List;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockWall;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 
 import cpw.mods.fml.relauncher.Side;
@@ -44,5 +49,15 @@ public class BlockCustomWall extends BlockWall
 	public IIcon getIcon(int side, int meta)
 	{
 		return block.getBlockTextureFromSide(0);
+	}
+
+	/**
+	 * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
+	 */
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void getSubBlocks(Item item, CreativeTabs tab, List list)
+	{
+		list.add(new ItemStack(item, 1, 0));
 	}
 }
