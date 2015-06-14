32c9d997f7205eed8c5f64b4548bff72cad043c6
Arona Jones <warlordjones@gmail.com>
2015-06-14 11:10:59 +0100

    Register slate stone/cobble in Oredict. Closes #222

--- src/main/java/steamcraft/common/compat/CompatabilityLayer.java
+++ src/main/java/steamcraft/common/compat/CompatabilityLayer.java
@@ -239,6 +239,13 @@ public class CompatabilityLayer
 
 		OreDictionary.registerOre("craftingHammer", new ItemStack(InitItems.itemHammer, 1, OreDictionary.WILDCARD_VALUE));
 		OreDictionary.registerOre("craftingHammer", new ItemStack(InitItems.itemBugHammer, 1, OreDictionary.WILDCARD_VALUE));
+
+		OreDictionary.registerOre("stone", new ItemStack(InitBlocks.blockSlate, 1, 1));
+		OreDictionary.registerOre("stone", new ItemStack(InitBlocks.blockSlate, 1, 2));
+		OreDictionary.registerOre("stone", new ItemStack(InitBlocks.blockSlate, 1, 3));
+		OreDictionary.registerOre("cobblestone", new ItemStack(InitBlocks.blockSlate, 1, 4));
+		OreDictionary.registerOre("cobblestone", new ItemStack(InitBlocks.blockSlate, 1, 5));
+		OreDictionary.registerOre("cobblestone", new ItemStack(InitBlocks.blockSlate, 1, 6));
 	}
 
 	private static void registerBiomeTypes()
