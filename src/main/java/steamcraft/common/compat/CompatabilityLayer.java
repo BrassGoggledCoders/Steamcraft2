41e17035d1c8ea4b16095ab0200a8da3e7d95cae
Decebaldecebal <rares_serban96@yahoo.com>
2015-06-26 16:26:46 +0300

    Register sulfur under the same name as Railcraft sulfur.Fixes #238

--- src/main/java/steamcraft/common/compat/CompatabilityLayer.java
+++ src/main/java/steamcraft/common/compat/CompatabilityLayer.java
@@ -207,7 +207,7 @@ public class CompatabilityLayer
 		OreDictHelper.registerOre("orePhosphate", InitBlocks.blockCustomOre, 6);
 
 		OreDictionary.registerOre("crystalEtherium", InitItems.itemResource);
-		OreDictHelper.registerOreWithAlts(InitItems.itemResource, 1, "powderSulfur", "sulfur", "dustSulfur", "powderSulphur", "sulphur", "dustSulphur");
+		OreDictHelper.registerOreWithAlts(InitItems.itemResource, 1, "powderSulfur", "sulfur", "dustSulfur");
 		OreDictHelper.registerOreWithAlts(InitItems.itemResource, 2, "shardObsidian", "slateObsidian");
 		OreDictHelper.registerOre("powderPhosphorus", InitItems.itemResource, 3);
 		OreDictHelper.registerOre("ingotUranium", InitItems.itemResource, 4);
