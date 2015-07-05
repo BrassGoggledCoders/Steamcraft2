e51c15fcf2ce3e395cfd73ab0a87776fa203eee7
Arona Jones <warlordjones@gmail.com>
2015-07-05 11:54:59 +0100

    Version & changelog updates for new release

--- src/main/java/steamcraft/common/init/InitRecipes.java
+++ src/main/java/steamcraft/common/init/InitRecipes.java
@@ -494,7 +494,7 @@ public class InitRecipes
 		GameRegistry.addRecipe(new ItemStack(InitItems.itemTeapot, 1, 8), "SLS", "LPL", "SLS", 'P', new ItemStack(InitItems.itemTeapot, 1, 2), 'L',
 				InitItems.itemTeaLeaf, 'S', Items.sugar);
 
-		GameRegistry.addRecipe(new ItemStack(InitItems.itemTeacup), "B B", " B ", 'B', Items.brick);
+		GameRegistry.addRecipe(new ItemStack(InitItems.itemTeacup), "B B", " B ", 'B', Items.clay);
 
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(InitItems.itemElectricJarSmall), "SIS", "GIG", "GGG", 'S',
 				InitItems.itemSlimeRubber, 'I', "ingotCopper", 'G', Blocks.glass));
