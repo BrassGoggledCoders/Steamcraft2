ae67976ea0a087dc7a277c2a2ae9226abf9e63ae
Arona Jones <warlordjones@gmail.com>
2015-08-21 11:54:05 +0100

    Improve charger model/texture

--- src/main/java/steamcraft/client/renderers/models/ModelCharger.java
+++ src/main/java/steamcraft/client/renderers/models/ModelCharger.java
@@ -18,63 +18,66 @@ import net.minecraft.entity.Entity;
 
 public class ModelCharger extends ModelBase
 {
-	public final ModelRenderer top;
-	public final ModelRenderer post1;
-	public final ModelRenderer post;
-	public final ModelRenderer post3;
-	public final ModelRenderer post2;
-	public final ModelRenderer base;
-	public final ModelRenderer base2;
+	public ModelRenderer base2;
+	public ModelRenderer top;
+	public ModelRenderer post;
+	public ModelRenderer post2;
+	public ModelRenderer post3;
+	public ModelRenderer post1;
+	public ModelRenderer base;
+	public ModelRenderer top2;
 
 	public ModelCharger()
 	{
-		this.top = new ModelRenderer(this);
-		this.top.addBox(0F, 0F, 0F, 15, 2, 15);
-		this.top.setRotationPoint(-7.5F, 6F, -7.5F);
-		this.top.mirror = true;
-		this.post1 = new ModelRenderer(this);
-		this.post1.addBox(0F, 0F, 0F, 2, 16, 2);
-		this.post1.setRotationPoint(-7F, 7F, -7F);
-		this.post1.mirror = true;
-		this.post = new ModelRenderer(this);
-		this.post.addBox(0F, 0F, 0F, 2, 16, 2);
-		this.post.setRotationPoint(5F, 8F, -7F);
-		this.post.mirror = true;
-		this.post3 = new ModelRenderer(this);
-		this.post3.addBox(0F, 0F, 0F, 2, 15, 2);
-		this.post3.setRotationPoint(5F, 8F, 5F);
-		this.post3.mirror = true;
-		this.post2 = new ModelRenderer(this);
-		this.post2.addBox(0F, 0F, 0F, 2, 16, 2);
-		this.post2.setRotationPoint(-7F, 7F, 5F);
-		this.post2.mirror = true;
-		this.base = new ModelRenderer(this);
-		this.base.addBox(0F, 0F, 0F, 16, 2, 16);
-		this.base.setRotationPoint(-8F, 22F, -8F);
-		this.base.mirror = true;
-		this.base2 = new ModelRenderer(this);
-		this.base2.addBox(0F, 0F, 0F, 12, 2, 12);
-		this.base2.setRotationPoint(-6F, 20F, -6F);
-		this.base2.mirror = true;
+		this.textureWidth = 64;
+		this.textureHeight = 32;
+		this.base2 = new ModelRenderer(this, 0, 18);
+		this.base2.setRotationPoint(-6.0F, 20.0F, -6.0F);
+		this.base2.addBox(0.0F, 0.0F, 0.0F, 12, 2, 12);
+		this.top2 = new ModelRenderer(this, 0, 18);
+		this.top2.setRotationPoint(-6.0F, 8.0F, -6.0F);
+		this.top2.addBox(0.0F, 0.0F, 0.0F, 12, 2, 12);
+		this.top = new ModelRenderer(this, 0, 0);
+		this.top.setRotationPoint(-8.0F, 6.0F, -8.0F);
+		this.top.addBox(0.0F, 0.0F, 0.0F, 16, 2, 16);
+		this.post = new ModelRenderer(this, 0, 0);
+		this.post.setRotationPoint(5.0F, 8.0F, -7.0F);
+		this.post.addBox(0.0F, 0.0F, 0.0F, 2, 16, 2);
+		this.post2 = new ModelRenderer(this, 0, 0);
+		this.post2.setRotationPoint(-7.0F, 7.0F, 5.0F);
+		this.post2.addBox(0.0F, 0.0F, 0.0F, 2, 16, 2);
+		this.post3 = new ModelRenderer(this, 0, 0);
+		this.post3.setRotationPoint(5.0F, 8.0F, 5.0F);
+		this.post3.addBox(0.0F, 0.0F, 0.0F, 2, 15, 2);
+		this.base = new ModelRenderer(this, 0, 0);
+		this.base.setRotationPoint(-8.0F, 22.0F, -8.0F);
+		this.base.addBox(0.0F, 0.0F, 0.0F, 16, 2, 16);
+		this.post1 = new ModelRenderer(this, 0, 0);
+		this.post1.setRotationPoint(-7.0F, 7.0F, -7.0F);
+		this.post1.addBox(0.0F, 0.0F, 0.0F, 2, 16, 2);
 	}
 
 	@Override
 	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
 	{
-		super.render(entity, f, f1, f2, f3, f4, f5);
-		this.setRotationAngles(f, f1, f2, f3, f4, f5);
-		this.top.render(f5);
-		this.post1.render(f5);
-		this.post.render(f5);
-		this.post3.render(f5);
-		this.post2.render(f5);
-		this.base.render(f5);
 		this.base2.render(f5);
+		this.top2.render(f5);
+		this.top.render(f5);
+		this.post.render(f5);
+		this.post2.render(f5);
+		this.post3.render(f5);
+		this.base.render(f5);
+		this.post1.render(f5);
 	}
 
-	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5)
+	/**
+	 * This is a helper function from Tabula to set the rotation of model parts
+	 */
+	public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z)
 	{
-		super.setRotationAngles(f, f1, f2, f3, f4, f5, null);
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
 	}
 
 }
