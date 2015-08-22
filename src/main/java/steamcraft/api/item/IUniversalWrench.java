fd604ca5aad13f915bb09674f7c3d893ae3d13ab
Arona Jones <warlordjones@gmail.com>
2015-08-22 10:19:40 +0100

    Well that's not going to work. (Revert commit e24eeb75954a45a46873688f7d12c16298d93139)

--- src/main/java/steamcraft/api/item/IUniversalWrench.java
+++ src/main/java/steamcraft/api/item/IUniversalWrench.java
@@ -8,10 +8,11 @@
  */
 package steamcraft.api.item;
 
-import buildcraft.api.tools.IToolWrench;
 import cofh.api.item.IToolHammer;
+
 import cpw.mods.fml.common.Optional;
-import mods.railcraft.api.core.items.IToolCrowbar;
+
+import buildcraft.api.tools.IToolWrench;
 
 /**
  * The Interface IUniversalWrench. Helper interface - implementing items will be compatible with Railcraft, MFR, BC and TE wrenches as well as Steamcraft's
@@ -19,11 +20,17 @@ import mods.railcraft.api.core.items.IToolCrowbar;
  *
  * @author warlordjones
  */
-@Optional.InterfaceList({
-		@Optional.Interface(iface = "buildcraft.api.tools.IToolWrench", modid = "BuildCraft|Core"),
-		@Optional.Interface(iface = "mods.railcraft.api.core.items.IToolCrowbar", modid = "Railcraft")
-})
-public interface IUniversalWrench extends IToolHammer, IToolWrench, IToolCrowbar
+@Optional.InterfaceList({ @Optional.Interface(iface = "buildcraft.api.tools.IToolWrench", modid = "BuildCraft|Core"),
+	// @Optional.Interface(iface = "mods.railcraft.api.core.items.IToolCrowbar",
+	// modid = "Railcraft"),
+	/*
+ * @Optional.Interface(iface = "powercrystals.minefactoryreloaded.api.IMFRHammer", modid = "MineFactoryReloaded")
+ */})
+public interface IUniversalWrench extends IToolHammer, IToolWrench// ,
+// IToolCrowbar
+/*
+ * , IMFRHammer
+ */
 {
 
 }
