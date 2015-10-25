62cff77c85f019fbee5b04a5dadd14c97f17f082
TheDoctorSoda <Unknown>
2015-10-25 13:47:45 -0700

    Entity Localisation and a couple derpfixes

--- src/main/java/steamcraft/common/init/InitEntities.java
+++ src/main/java/steamcraft/common/init/InitEntities.java
@@ -12,12 +12,9 @@
  */
 package steamcraft.common.init;
 
-import net.minecraft.entity.EnumCreatureType;
-
 import cpw.mods.fml.common.registry.EntityRegistry;
-
+import net.minecraft.entity.EnumCreatureType;
 import net.minecraftforge.common.BiomeDictionary;
-
 import steamcraft.common.Steamcraft;
 import steamcraft.common.entities.EntityFallingBoulder;
 import steamcraft.common.entities.EntityTimeBomb;
@@ -42,10 +39,8 @@ import steamcraft.common.entities.projectile.EntitySplashLightningBottle;
  * @author Surseance
  *
  */
-public class InitEntities
-{
-	public static void init()
-	{
+public class InitEntities {
+	public static void init() {
 		int entID = 0;
 
 		// Mobs
@@ -59,16 +54,15 @@ public class InitEntities
 		EntityRegistry.registerModEntity(EntitySpiderQueen.class, "SpiderQueen", entID++, Steamcraft.instance, 64, 1, true);
 		EntityRegistry.registerModEntity(EntityShroomZombie.class, "ShroomZombie", entID++, Steamcraft.instance, 64, 1, true);
 		EntityRegistry.registerModEntity(EntityShroomSkeleton.class, "ShroomSkeleton", entID++, Steamcraft.instance, 64, 1, true);
-		EntityRegistry.registerModEntity(EntityAbandonedGolem.class, "AbdandonedGolem", entID++, Steamcraft.instance, 64, 1, true);
+		EntityRegistry.registerModEntity(EntityAbandonedGolem.class, "AbandonedGolem", entID++, Steamcraft.instance, 64, 1, true);
 		EntityRegistry.registerModEntity(EntityWhale.class, "Whale", entID++, Steamcraft.instance, 64, 1, true);
-		EntityRegistry.addSpawn(EntityWhale.class, 1, 1, 1, EnumCreatureType.waterCreature,
-				BiomeDictionary.getBiomesForType(BiomeDictionary.Type.OCEAN));
+		EntityRegistry.addSpawn(EntityWhale.class, 1, 1, 1, EnumCreatureType.waterCreature, BiomeDictionary.getBiomesForType(BiomeDictionary.Type.OCEAN));
 		// Non-Living
 		// Projectile
 		EntityRegistry.registerModEntity(EntityBullet.class, "Bullet", entID++, Steamcraft.instance, 40, 1, true);
 		EntityRegistry.registerModEntity(EntityRocket.class, "Rocket", entID++, Steamcraft.instance, 40, 1, true);
 		EntityRegistry.registerModEntity(EntitySplashLightningBottle.class, "SplashLightningBottle", entID++, Steamcraft.instance, 40, 1, true);
-		EntityRegistry.registerModEntity(EntityFieldManipulator.class, "SplashLightningBottle", entID++, Steamcraft.instance, 40, 1, true);
+		EntityRegistry.registerModEntity(EntityFieldManipulator.class, "FieldManipulator", entID++, Steamcraft.instance, 40, 1, true);
 		// Block
 		EntityRegistry.registerModEntity(EntityTimeBomb.class, "TimeBomb", entID++, Steamcraft.instance, 40, 1, true);
 		EntityRegistry.registerModEntity(EntityFallingBoulder.class, "FallingBoulder", entID++, Steamcraft.instance, 40, 1, true);
