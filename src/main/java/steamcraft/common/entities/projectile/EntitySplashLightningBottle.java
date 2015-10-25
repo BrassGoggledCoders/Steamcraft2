f923a98e7f889d7ac30d97bcb95d44f09c6bafae
TheDoctorSoda <Unknown>
2015-10-25 13:56:49 -0700

    Revert "Entity Localisation and a couple derpfixes"
    This reverts commit 62cff77c85f019fbee5b04a5dadd14c97f17f082.

--- src/main/java/steamcraft/common/entities/projectile/EntitySplashLightningBottle.java
+++ src/main/java/steamcraft/common/entities/projectile/EntitySplashLightningBottle.java
@@ -14,57 +14,71 @@ package steamcraft.common.entities.projectile;
 
 import java.util.List;
 
-import boilerplate.common.baseclasses.BaseThrowableEntity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
+
+import boilerplate.common.baseclasses.BaseThrowableEntity;
 import steamcraft.common.lib.DamageSourceHandler;
 
-public class EntitySplashLightningBottle extends BaseThrowableEntity {
-	public EntitySplashLightningBottle(World world, EntityLivingBase entityLiving) {
+public class EntitySplashLightningBottle extends BaseThrowableEntity
+{
+	public EntitySplashLightningBottle(World world, EntityLivingBase entityLiving)
+	{
 		super(world, entityLiving);
 	}
 
-	public EntitySplashLightningBottle(World world) {
+	public EntitySplashLightningBottle(World world)
+	{
 		super(world);
 		this.setSize(0.5F, 0.5F);
 	}
 
 	@Override
-	protected float getGravityVelocity() {
+	protected float getGravityVelocity()
+	{
 		return 0.03F;
 	}
 
 	@SuppressWarnings({ "rawtypes" })
 	@Override
-	protected void onImpact(MovingObjectPosition mop) {
-		if (mop.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK) {
-			if (!this.worldObj.isRemote) {
+	protected void onImpact(MovingObjectPosition mop)
+	{
+		if (mop.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK)
+		{
+			if (!this.worldObj.isRemote)
+			{
 				AxisAlignedBB axisalignedbb = this.boundingBox.expand(4.0D, 2.0D, 4.0D);
 				List list1 = this.worldObj.getEntitiesWithinAABB(EntityLivingBase.class, axisalignedbb);
 
-				if ((list1 != null) && !list1.isEmpty()) {
-					for (Object obj : list1) {
+				if ((list1 != null) && !list1.isEmpty())
+				{
+					for (Object obj : list1)
+					{
 						EntityLivingBase entitylivingbase = (EntityLivingBase) obj;
 						double d0 = this.getDistanceSqToEntity(entitylivingbase);
 
-						if (d0 < 16.0D) {
+						if (d0 < 16.0D)
+						{
 							double d1 = 1.0D - (Math.sqrt(d0) / 4.0D);
 
-							if (entitylivingbase == mop.entityHit) {
+							if (entitylivingbase == mop.entityHit)
+							{
 								d1 = 1.0D;
 							}
 							entitylivingbase.attackEntityFrom(DamageSourceHandler.electrocution, 4);
 
-							if (entitylivingbase instanceof EntityCreeper) {
+							if (entitylivingbase instanceof EntityCreeper)
+							{
 								EntityCreeper creeper = (EntityCreeper) entitylivingbase;
 								creeper.getDataWatcher().updateObject(17, Byte.valueOf((byte) (1)));
 							}
 						}
 					}
 				}
+
 				this.worldObj.playAuxSFX(2002, (int) Math.round(this.posX), (int) Math.round(this.posY), (int) Math.round(this.posZ), 0);
 				this.setDead();
 			}
