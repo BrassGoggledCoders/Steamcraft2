1b355e67ce1f911375ad46e4a1b6ff6d166dae40
TheDoctorSoda <thedoctorsoda@gmail.com>
2015-10-26 16:55:03 -0700

    Configuration for golem dropping cast iron from fire

--- src/main/java/steamcraft/common/lib/events/EventHandlerMobDrop.java
+++ src/main/java/steamcraft/common/lib/events/EventHandlerMobDrop.java
@@ -9,6 +9,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import steamcraft.common.config.ConfigGeneral;
 import steamcraft.common.init.InitItems;
 
 public class EventHandlerMobDrop {
@@ -17,18 +18,16 @@ public class EventHandlerMobDrop {
 
 	@SubscribeEvent
 	public void onMobDrops(LivingDropsEvent event) {
-		if (event.entity instanceof EntityIronGolem) {
-			if (event.entity.isBurning()) {
-				event.drops.clear();
-				ItemStack ingot = new ItemStack(InitItems.itemIngot, 3 + this.rand.nextInt(3), 7);
-				ItemStack poppy = new ItemStack(Item.getItemFromBlock(Blocks.red_flower), this.rand.nextInt(3), 0);
+		if (event.entity instanceof EntityIronGolem && event.entity.isBurning() && ConfigGeneral.golemFireDrop) {
+			event.drops.clear();
+			ItemStack ingot = new ItemStack(InitItems.itemIngot, 3 + this.rand.nextInt(3), 7);
+			ItemStack poppy = new ItemStack(Item.getItemFromBlock(Blocks.red_flower), this.rand.nextInt(3), 0);
 
-				EntityItem ingotDrop = new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, ingot);
-				EntityItem poppyDrop = new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, poppy);
+			EntityItem ingotDrop = new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, ingot);
+			EntityItem poppyDrop = new EntityItem(event.entity.worldObj, event.entity.posX, event.entity.posY, event.entity.posZ, poppy);
 
-				event.drops.add(ingotDrop);
-				event.drops.add(poppyDrop);
-			}
+			event.drops.add(ingotDrop);
+			event.drops.add(poppyDrop);
 		}
 	}
 }
