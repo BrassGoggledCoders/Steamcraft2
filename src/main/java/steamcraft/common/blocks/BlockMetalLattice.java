e45d4f4e0b39f627d710c1f067df09ec446bc18b
Arona Jones <warlordjones@gmail.com>
2015-11-05 19:42:32 +0000

    Make both scaffold and lattice useable as ladders

--- src/main/java/steamcraft/common/blocks/BlockMetalLattice.java
+++ src/main/java/steamcraft/common/blocks/BlockMetalLattice.java
@@ -18,9 +18,11 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -100,5 +102,11 @@ public class BlockMetalLattice extends BaseMetadataBlock
 	public int getRenderType()
 	{
 		return RenderIDs.blockTransWithInsideRI;
+	}
+
+	@Override
+	public boolean isLadder(IBlockAccess world, int x, int y, int z, EntityLivingBase entity)
+	{
+		return true;
 	}
 }
--- src/main/java/steamcraft/common/blocks/BlockScaffold.java
+++ src/main/java/steamcraft/common/blocks/BlockScaffold.java
@@ -18,9 +18,11 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -99,5 +101,11 @@ public class BlockScaffold extends BaseMetadataBlock
 	public int getRenderType()
 	{
 		return RenderIDs.blockTransWithInsideRI;
+	}
+
+	@Override
+	public boolean isLadder(IBlockAccess world, int x, int y, int z, EntityLivingBase entity)
+	{
+		return true;
 	}
 }
