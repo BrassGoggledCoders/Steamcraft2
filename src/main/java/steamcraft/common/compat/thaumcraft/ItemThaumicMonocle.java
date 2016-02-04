a5f033682b801bd706bcff6715a3a97032a68da8
Arona Jones <warlordjones@gmail.com>
2016-02-04 19:49:02 +0000

    Repackage common.items.compat to common.compat.thaumcraft for consistency

--- src/main/java/steamcraft/common/compat/thaumcraft/ItemThaumicMonocle.java
+++ src/main/java/steamcraft/common/compat/thaumcraft/ItemThaumicMonocle.java
@@ -10,7 +10,7 @@
  * (http://www.minecraftforum.net/topic/251532-181-steamcraft-source-code-releasedmlv054wip/)
  *
  */
-package steamcraft.common.items.compat;
+package steamcraft.common.compat.thaumcraft;
 
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
