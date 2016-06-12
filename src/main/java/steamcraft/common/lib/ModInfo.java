dfd1d808ba9b44a47c7f4e6b19f89193668e01f4
warlordjones <warlordjones@gmail.com>
2016-06-12 19:56:41 +0100

    Update version and changelog

--- src/main/java/steamcraft/common/lib/ModInfo.java
+++ src/main/java/steamcraft/common/lib/ModInfo.java
@@ -6,7 +6,7 @@ public class ModInfo
 	public static final String ID = "steamcraft2";
 	public static final String PREFIX = "steamcraft:";
 	public static final String NAME = "Steamcraft 2";
-	public static final String VERSION = "B5.0.1";
+	public static final String VERSION = "B5.0.2";
 
 	public static final String CLIENT_PROXY = "steamcraft.client.ClientProxy";
 	public static final String COMMON_PROXY = "steamcraft.common.CommonProxy";
